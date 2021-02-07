208406ce7e873792e51d501c6ad6e146b0c0f121
Situu <70975136+Jad-Kh@users.noreply.github.com>
2021-02-07 19:43:20 +0200

    Navbar Header Update

--- homepage.blade.php
+++ homepage.blade.php
@@ -16,12 +16,11 @@
 
     <!-- Css Styles -->
     <link rel="stylesheet" href="plugins/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="plugins/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="plugins/css/elegant-icons.css" type="text/css">
     <link rel="stylesheet" href="plugins/css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="plugins/css/barfiller.css" type="text/css">
     <link rel="stylesheet" href="plugins/css/magnific-popup.css" type="text/css">
     <link rel="stylesheet" href="plugins/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/homepage.css" type="text/css">
     <link rel="stylesheet" href="css/preloader.css" type="text/css">
     <link rel="stylesheet" href="css/upperpart.css" type="text/css">
@@ -40,7 +39,7 @@
                 <div class="row">
                     <div class="col-lg-6 col-md-8">
                         <div id="widget">
-                            <ul>
+                            <ul>  <!-- Date & Sign up Switch -->
                                 <li><i class="fa fa-clock-o"></i>Feb 21, 2021</li>
                                 <li id="signup-switch"><i class="fa fa-sign-out"></i> Login / Sign up </li>
                             </ul>
@@ -51,12 +50,119 @@
         </div>
         <div class="logo">
             <div class="container">
-                <div class="row">
+                <div class="row"> <!-- Header Logo -->
                     <div class="col-lg-12 text-center">
                        <a href="#"><img src="img/logo.png"></a>
                     </div>
                 </div>
             </div>
+        </div> <!-- Buttons Bar -->
+        <div class="buttons-navbar">
+            <div class="container">
+                <div id="header-bar header-open">
+                    <i class="fa fa-bars"></i>
+                </div> <!-- Search Option -->
+                <div class="nav-search search-switch">
+                    <i class="fa fa-search"></i>
+                </div> <!-- Buttons -->
+                <div class="buttons-menu">
+                    <ul>
+                        <li><a href="#"><span>Home</span></a></li>
+                        <li class="mega-menu"><a href="#"><span>Quicknews <i class="fa fa-angle-down"></i></span></a>
+                            <!-- megamenu -->
+                            <div class="megamenu-wrapper">
+                                <div class="mw-post">
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-1.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">A Monster Prom poster got hijacked for a Papa Roach
+                                                    concert...</a></h6>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-2.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">A new Borderlands 3 trailer introduces Moze and her...</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-3.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Teamfight Tactics is in chaos after today's patch...</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-4.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Borderlands 2 dev explains why there's mysterious
+                                                    boxes...</a></h6>
+                                        </div>
+                                    </div>
+                                    <div class="mw-post-item">
+                                        <div class="mw-pic">
+                                            <img src="img/megamenu/mm-5.jpg" alt="">
+                                        </div>
+                                        <div class="mw-text">
+                                            <h6><a href="#">Capcom asks select fans to test new Resident Evil game</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#"><span>Pages <i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown">
+                                <li><a href="#">Activity</a></li>
+                                <li><a href="#">Articles</a></li>
+                                <li><a href="#">Game Menu</a></li>
+                                <li><a href="#">Gallery</a></li>
+                                <li><a href="#">Reviews</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>User <i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown">
+                                <li><a href="#">Lists</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Screenshots</a></li>
+                                <li><a href="#">Link-Accounts</a></li>
+                                <li><a href="#">User info</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span>Schedule <i class="fa fa-angle-down"></i></span></a>
+                            <ul class="dropdown">
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Calendar</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><span>Videos <i class="fa fa-angle-down"></i></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Search Model -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
         </div>
     </div>
 
@@ -70,6 +176,7 @@
     <script src="plugins/js/owl.carousel.min.js"></script>
     <script src="js/preloader.js"></script>
     <script src="js/homepage.js"></script>
+    <script src="js/upperpart.js"></script>
 
 </body>
 </html>
