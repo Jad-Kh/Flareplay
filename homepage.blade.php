daaad032b95ce5b69c9f7784f7c1014101e2839e
Situu <70975136+Jad-Kh@users.noreply.github.com>
2021-02-07 14:48:01 +0200

    Header Code

--- homepage.blade.php
+++ homepage.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Flareplay</title>
- 
+
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
         rel="stylesheet">
@@ -24,14 +24,42 @@
     <link rel="stylesheet" href="plugins/css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/homepage.css" type="text/css">
     <link rel="stylesheet" href="css/preloader.css" type="text/css">
+    <link rel="stylesheet" href="css/upperpart.css" type="text/css">
 </head>
 
 <body>
     <!-- Page Preloader --> 
     <div id="preloader">
-        <div class="loader">    
+        <div class="loader"></div>    
     </div> 
-    
+
+    <!-- Header Menu -->
+    <div class="header">
+        <div id="navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8">
+                        <div id="widget">
+                            <ul>
+                                <li><i class="fa fa-clock-o"></i>Feb 21, 2021</li>
+                                <li id="signup-switch"><i class="fa fa-sign-out"></i> Login / Sign up </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+        </div>
+        <div class="logo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                       <a href="#"><img src="img/logo.png"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Js Plugins -->
     <script src="plugins/js/jquery-3.3.1.min.js"></script>
     <script src="plugins/js/bootstrap.min.js"></script>
